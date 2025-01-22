d523b5263b7d4228c1aad375d7eae5d8355807ec
Nikita kale <nkale0829@gmail.com>
2025-01-22 20:03:54 +0530

    Delete community_volunteering-main_(1)[1]/community_volunteering-main directory

--- community_volunteering-main_(1)[1]/community_volunteering-main/src/main/java/com/example/demo/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.TaskEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
-}
